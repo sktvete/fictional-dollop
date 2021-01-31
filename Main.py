d0f83c9b815278a218fa8ac847a96b73282babb0
sktvete <78177784+sktvete@users.noreply.github.com>
2021-01-31 12:52:13 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,17 +1,54 @@
+import random
+import time
 
 
+def blackjack():
+    total = 0
+    dealertotal = 0
+    penger = 100
 
-print(32)
+    kort = random.randint(1, 13)
+    print("Du fikk", kort)
+    total += kort
 
-x = 2
+    kort = random.randint(1, 13)
+    print("Du fikk", kort)
+    total += kort
 
-if x == 5:
-    print(333)
-else:
-    print(1111)
+    print("Total =", total)
+
+    while total < 21:
+        inp = input("Vil du trekke nytt? y/n ")
+        if inp == "n":
+            break
+        kort = random.randint(1, 13)
+        print("Du fikk", kort)
+        total += kort
+        print("Total =", total)
+
+    if total > 21:
+        print("Du tapte")
+
+    else:
+        while dealertotal < 17:
+            time.sleep(1)
+            kort = random.randint(1, 13)
+            print("Dealer trakk", kort)
+            dealertotal += kort
+            print(dealertotal)
+
+        if dealertotal > 21 or total > dealertotal:
+            print("Du vant!")
+        elif total == dealertotal:
+            print("Likt")
+        else:
+            print("Du tapte:\(")
 
 
-
+while True:
+    blackjack()
+    time.sleep(1)
+    print("\n"*2)
 
 
 
