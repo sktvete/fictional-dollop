037d159d153244f56a3844f28d61cefad039e1da
Sindre <sktvete@gmail.com>
2021-02-03 17:00:32 +0100

    :)

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-f=int(input("sd "))
-y=4
-z=f-y
-print(z)
+import random
+import time
+
+listLower = [100]
+listHigher = [0]
+
+guess = 0
+#inp = int(input("Your number? "))
+inp = random.randint(1, 100)
+print("Your number is", inp)
+time.sleep(1)
+
+
+
+while True:
+    guess =
