e1982c5b45541e3749dd8fdd4266d5c5b682ced9
sktve <sktvete@gmail.com>
2021-02-06 23:40:14 +0100

    l

--- test.py
+++ test.py
@@ -4,20 +4,25 @@ import time
 lstLower = [100]
 lstHigher = [0]
 
+tries = 0
 guess = 0
-#inp = int(input("Your number? "))
-inp = random.randint(1, 100) # max(lstLower)
+inp = int(input("Your number? "))
+#inp = random.randint(1, 100) # max(lstLower)
 print("Your number is", inp) # min(lstLower)
 
 while True:
     time.sleep(1)
+    guess = ((min(lstLower) + max(lstHigher)) // 2)
+    tries += 1
     print(guess)
-    if guess < inp:
-        guess = ((min(lstLower) + max(lstHigher)) / 2)
-        lstHigher.append(guess)
-    elif guess > inp:
-        guess = ((min(lstLower) + max(lstHigher)) / 2)
+
+    if guess > inp:
         lstLower.append(guess)
+    elif guess < inp:
+        lstHigher.append(guess)
+    else:
+        print("It took", tries, "tries")
+        break
 
 
 
